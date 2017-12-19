f39dbd05cc118e3744fb8fb94467276baa718cca
Aaron Lun <alun@wehi.edu.au>
2017-12-19 18:12:44 +0000

    Renamed downsampling function, updated docs and tests.

---
+++ R/downsampleMatrix.R
@@ -0,0 +1,15 @@
+downsampleMatrix <- function(x, prop, bycol=TRUE) 
+# Downsamples the count matrix to the specified proportion.
+# This is done on a per-cell basis, possibly with cell-specific 'prop',
+# unless 'bycol=FALSE', in which case it is done for the entire matrix.
+#
+# written by Aaron Lun
+# created 18 December 2017    
+{
+    if (bycol) {
+        prop <- rep(prop, length.out = ncol(x))
+    }
+    out <- .Call(cxx_downsample_matrix, x, prop, bycol)
+    dimnames(out) <- dimnames(x)
+    return(out)
+}
--- tests/testthat/test-downsample.R
+++ tests/testthat/test-downsample.R
@@ -1,28 +1,42 @@
-# Testing the downsampleCounts function.
+# Testing the downsampleMatrix function.
 # library(DropletUtils); library(testthat); source("test-downsample.R")
 
-test_that("downsampling is correct", {
-    CHECKFUN <- function(input, prop) {
-        out <- scater:::downsampleCounts(input, prop)
-        expect_identical(colSums(out), round(colSums(input)*prop))
-        expect_true(all(out <= input))
-        return(invisible(NULL))
-    }
+CHECKFUN <- function(input, prop) {
+    out <- downsampleMatrix(input, prop)
+    expect_identical(colSums(out), round(colSums(input)*prop))
+    expect_true(all(out <= input))
+    return(invisible(NULL))
+}
 
+CHECKSUM <- function(input, prop) {
+    expect_equal(sum(downsampleMatrix(input, prop, bycol=FALSE)), round(prop*sum(input)))
+    return(invisible(NULL))
+}
+
+test_that("downsampling from a count matrix gives expected sums", {
     # Vanilla run.
     set.seed(0)
     ncells <- 100
     u1 <- matrix(rpois(20000, 5), ncol=ncells)
+    u2 <- matrix(rpois(20000, 1), ncol=ncells)
+
     set.seed(100)
     CHECKFUN(u1, 0.111) # Avoid problems with different rounding of 0.5.
-    CHECKFUN(u1, 0.333) # Avoid problems with different rounding of 0.5.
-    CHECKFUN(u1, 0.777) # Avoid problems with different rounding of 0.5.
+    CHECKFUN(u1, 0.333)
+    CHECKFUN(u1, 0.777)
+    
+    CHECKSUM(u1, 0.111) 
+    CHECKSUM(u1, 0.333)
+    CHECKSUM(u1, 0.777)
 
-    u2 <- matrix(rpois(20000, 1), ncol=ncells)
     set.seed(101)
     CHECKFUN(u2, 0.111) # Avoid problems with different rounding of 0.5.
-    CHECKFUN(u2, 0.333) # Avoid problems with different rounding of 0.5.
-    CHECKFUN(u2, 0.777) # Avoid problems with different rounding of 0.5.
+    CHECKFUN(u2, 0.333)
+    CHECKFUN(u2, 0.777)
+
+    CHECKSUM(u2, 0.111) 
+    CHECKSUM(u2, 0.333)
+    CHECKSUM(u2, 0.777)
 
     # Checking double-precision inputs.
     v1 <- u1
@@ -32,12 +46,20 @@ test_that("downsampling is correct", {
     CHECKFUN(v1, 0.333)
     CHECKFUN(v1, 0.777)
 
+    CHECKSUM(v1, 0.111)
+    CHECKSUM(v1, 0.333)
+    CHECKSUM(v1, 0.777)
+
     v2 <- u2
     storage.mode(v2) <- "double"
     set.seed(202)
     CHECKFUN(v2, 0.111)
     CHECKFUN(v2, 0.333)
     CHECKFUN(v2, 0.777)
+    
+    CHECKSUM(v2, 0.111)
+    CHECKSUM(v2, 0.333)
+    CHECKSUM(v2, 0.777)
 
     # Checking vectors of proportions.
     set.seed(300)
@@ -58,24 +80,46 @@ test_that("downsampling is correct", {
     CHECKFUN(w1, 0.333)
     CHECKFUN(w1, 0.777)
 
+    CHECKSUM(w1, 0.111)
+    CHECKSUM(w1, 0.333)
+    CHECKSUM(w1, 0.777)
+
     w2 <- as(v2, "dgCMatrix")
     set.seed(404)
     CHECKFUN(w2, 0.111)
     CHECKFUN(w2, 0.333)
     CHECKFUN(w2, 0.777)
 
+    CHECKSUM(w2, 0.111)
+    CHECKSUM(w2, 0.333)
+    CHECKSUM(w2, 0.777)
+})
+
+test_that("downsampling from a count matrix gives expected margins", {
     # Checking that the sampling scheme is correct (as much as possible).
     set.seed(500)
     known <- matrix(1:5, nrow=5, ncol=10000)
     prop <- 0.51
     truth <- known[,1]*prop
-    out <- scater:::downsampleCounts(known, prop)
+    out <- downsampleMatrix(known, prop)
     expect_true(all(abs(rowMeans(out)/truth - 1) < 0.1)) # Less than 10% error on the estimated proportions.
 
-    known <- matrix(1:5*10, nrow=5, ncol=10000)
+    out <- downsampleMatrix(known, prop, bycol=FALSE) # Repeating by column.
+    expect_true(all(abs(rowMeans(out)/truth - 1) < 0.1)) 
+
+    # Repeating with larger counts.
+    known <- matrix(1:5*100, nrow=5, ncol=10000)
     prop <- 0.51
     truth <- known[,1]*prop
-    out <- scater:::downsampleCounts(known, prop)
+    out <- downsampleMatrix(known, prop)
     expect_true(all(abs(rowMeans(out)/truth - 1) < 0.01)) # Less than 1% error on the estimated proportions.
+
+    out <- downsampleMatrix(known, prop, bycol=FALSE)
+    expect_true(all(abs(rowMeans(out)/truth - 1) < 0.01)) 
+
+    # Checking the column sums when bycol=FALSE.
+    known <- matrix(100, nrow=1000, ncol=10)
+    out <- downsampleMatrix(known, prop, bycol=FALSE)
+    expect_true(all(abs(colMeans(out)/colMeans(known)/prop - 1) < 0.01))
 })
 
