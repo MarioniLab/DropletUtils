d9f613450b71754ada9ff26795d2e7d51526e31f
Aaron Lun <alun@wehi.edu.au>
2017-12-19 18:45:56 +0000

    Added downsampleReads function, for read-based downsampling.

---
+++ R/downsampleReads.R
@@ -0,0 +1,33 @@
+downsampleReads <- function(sample, barcode.length, prop, bycol=FALSE) 
+# Downsamples the reads to produce a matrix of UMI counts, given
+# a HDF5 file containing molecule information.
+#
+# written by Aaron Lun
+# created 19 December 2017    
+{
+    incoming <- .readHDF5Data(sample, barcode.length)
+    cell.id <- paste0(incoming$data$cell, "-", incoming$data$gem_group)
+    all.cells <- sort(unique(cell.id))
+    
+    # Obtaining new read counts for each molecule.
+    o <- order(cell.id)
+    cell.id <- cell.id[o]
+    rout <- rle(cell.id)
+
+    if (bycol) {
+        prop <- rep(prop, length.out=length(all.cells))
+    }
+    new.read.counts <- .Call(cxx_downsample_runs, rout$lengths, incoming$data$reads[o], prop, bycol)
+
+    # Assembling into a sparse matrix.
+    keep <- new.read.counts>0L
+    rid <- incoming$data$gene[o[keep]]
+    cid <- match(cell.id[keep], all.cells)
+
+    out <- sparseMatrix(i=rid, j=cid, x=rep(1, length(rid)),
+                        dims=c(length(incoming$genes), length(all.cells)),
+                        use.last.ij=FALSE, giveCsparse=TRUE)
+    rownames(out) <- incoming$genes
+    colnames(out) <- all.cells
+    return(out)
+}
