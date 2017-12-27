ab8e4e927bac56c24313e3df92dd6d987bfbeefd
Aaron Lun <alun@wehi.edu.au>
2017-12-27 15:26:44 +0000

    Updated tests for multinomial model in emptyDrops.

--- tests/testthat/test-emptydrops.R
+++ tests/testthat/test-emptydrops.R
@@ -1,45 +1,94 @@
 # This tests that the C++ code for emptyDrops does what it says it does.
 # library(DropletUtils); library(testthat); source("test-emptydrops.R")
 
-test_that("simulated deviance calculations are correct", {
-    set.seed(100)
-    ambient.prop <- runif(1000)
-    ambient.prop <- ambient.prop/sum(ambient.prop)
-
-    set.seed(200)
-    sim.totals <- seq(1, 2000, by=0.5)
-    sim.LR <- numeric(length(sim.totals))
-    for (x in seq_along(sim.LR)) {
-        cur.means <- ambient.prop*sim.totals[x]
-        current <- rpois(length(ambient.prop), lambda=cur.means)
-        sim.LR[x] <- sum(current * log(current/cur.means), na.rm=TRUE) + sum(cur.means - current)
-    }
-
-    set.seed(200)
-    ref <- DropletUtils:::.simulate_dev(sim.totals, ambient.prop) 
-    expect_equal(sim.LR, ref)             
-})
-
 test_that("p-value calculations are correct", {
-    ncells <- 10000
-    obs.totals <- sample(1000, ncells, replace=TRUE) + 10
-    obs.spread <- runif(ncells)
-    sim.totals <- seq(0, 2000, by=0.5)
-    sim.spread <- runif(length(sim.totals))
-
-    span <- sqrt(2)
-    p <- numeric(length(obs.spread))
-    limited <- logical(length(obs.spread))
-    for (x in seq_along(p)) {
-        current <- obs.totals[x]/span <= sim.totals & obs.totals[x]*span >= sim.totals
-        nexceed <- sum(sim.spread[current] >= obs.spread[x])
-        limited[x] <- nexceed==0L
-        p[x] <- (nexceed + 1)/(sum(current)+1)
+    # Simulating some counts.
+    SIMSTUFF <- function(nbarcodes, ngenes) { 
+        totals <- sample(1000, nbarcodes, replace=TRUE)
+        ambient <- runif(ngenes)
+        probs <- numeric(length(totals))
+        for (i in seq_along(totals)) { 
+            sim <- rmultinom(1, totals[i], prob=ambient)
+            probs[i] <- sum(sim * log(ambient) - lfactorial(sim))
+        }
+        return(list(totals=totals, probs=probs, ambient=ambient))
     }
-    
-    stats <- DropletUtils:::.compute_P(obs.totals, obs.spread, sim.totals, sim.spread, span=log2(span))
-    expect_equal(stats$p.value, p)
-    expect_identical(stats$limited, limited)
+
+    # Setting up the reference function.
+    REFFUN <- function(totals, probs, ambient, iter) {
+        cumamb <- cumsum(ambient)
+        logamb <- log(ambient)
+        sumamb <- sum(ambient)
+
+        n.above <- integer(length(totals))
+        for (it in seq_len(iter)) {
+            vec <- runif(max(totals), 0, sumamb)
+            chosen <- findInterval(vec, cumamb)+1L
+            
+            for (i in seq_along(totals)) {
+                curchosen <- chosen[seq_len(totals[i])]
+                curprof <- tabulate(curchosen, nbins=length(ambient))
+                curP <- sum(logamb * curprof - lfactorial(curprof))
+                if (curP <= probs[i]) { 
+                    n.above[i] <- n.above[i] + 1L
+                }
+            }
+        }
+
+        return(n.above)
+    }
+
+    # Checking the equivalence of our two implementations.
+    set.seed(0)
+    sim <- SIMSTUFF(100, 20)
+    totals <- sim$totals
+    probs <- sim$probs
+    ambient.prof <- sim$ambient
+
+    set.seed(100)
+    ref <- REFFUN(totals, probs, ambient.prof, iter=100)
+    set.seed(100)
+    stats <- DropletUtils:::.permute_counter(totals, probs, ambient.prof, iter=100)
+    expect_identical(stats, ref)
+
+    # More barcodes (especially checking what happens with two barcodes of the same total).
+    set.seed(0)
+    sim <- SIMSTUFF(1001, 20)
+    totals <- sim$totals
+    probs <- sim$probs
+    ambient.prof <- sim$ambient
+
+    set.seed(100)
+    ref <- REFFUN(totals, probs, ambient.prof, iter=100)
+    set.seed(100)
+    stats <- DropletUtils:::.permute_counter(totals, probs, ambient.prof, iter=100)
+    expect_identical(stats, ref)
+
+    # More genes.
+    set.seed(0)
+    sim <- SIMSTUFF(100, 200)
+    totals <- sim$totals
+    probs <- sim$probs
+    ambient.prof <- sim$ambient
+
+    set.seed(100)
+    ref <- REFFUN(totals, probs, ambient.prof, iter=100)
+    set.seed(100)
+    stats <- DropletUtils:::.permute_counter(totals, probs, ambient.prof, iter=100)
+    expect_identical(stats, ref)
+
+    # More iterations.
+    set.seed(0)
+    sim <- SIMSTUFF(100, 20)
+    totals <- sim$totals
+    probs <- sim$probs
+    ambient.prof <- sim$ambient
+
+    set.seed(100)
+    ref <- REFFUN(totals, probs, ambient.prof, iter=500)
+    set.seed(100)
+    stats <- DropletUtils:::.permute_counter(totals, probs, ambient.prof, iter=500)
+    expect_identical(stats, ref)
 })
 
 test_that("emptyDrops runs to completion", {
@@ -49,9 +98,18 @@ test_that("emptyDrops runs to completion", {
     e.out <- emptyDrops(my.counts, lower=limit, scale=1)
     
     totals <- colSums(my.counts)
-    expect_identical(totals, e.out$Total)
-    expect_true(all(is.na(e.out$Deviance[totals<=limit])))
-    expect_true(all(!is.na(e.out$Deviance[totals>limit])))
+    expect_identical(as.integer(totals), e.out$Total)
+    expect_true(all(is.na(e.out$LogProb[totals<=limit])))
+    expect_true(all(!is.na(e.out$LogProb[totals>limit])))
+
+    # Checking that the log-probability calculation is correct.
+    ambient.prop <- edgeR::goodTuringProportions(Matrix::rowSums(my.counts[,totals<= limit]))
+    valid <- which(totals > limit)
+    collected <- numeric(nrow(e.out))
+    for (v in valid) { 
+        collected[v] <- dmultinom(my.counts[,v], size=totals[v], prob=ambient.prop, log=TRUE)
+    }
+    expect_equal(e.out$LogProb[valid], collected[valid])
 
     # Testing barcode ranks.    
     brout <- barcodeRanks(my.counts, lower=limit)
@@ -67,7 +125,7 @@ test_that("emptyDrops runs to completion", {
     e.out2 <- emptyDrops(my.counts, test.args=list(test.ambient=TRUE))
     expect_identical(e.out$Total, e.out2$Total)
     expect_identical(e.out$Deviance[totals>limit], e.out2$Deviance[totals>limit])
-    expect_true(all(!is.na(e.out2$Deviance[totals>0])))
+    expect_true(all(!is.na(e.out2$LogProb[totals>0])))
 
     # Checking scaling options.
     e.out <- emptyDrops(my.counts, scale=0.6)
