5db17374d7dff92c544fcccbef1f644c212892b5
Jonathan Griffiths <jonathan.griffiths.94@gmail.com>
2018-01-04 16:37:19 +0000

    Maybe all the changes now

--- R/defaultDrops.R
+++ R/defaultDrops.R
@@ -1,23 +1,24 @@
-defaultDrops <- function(m, exp.cells = 3000, upper.pct = 0.99, lower.prop = 0.1)
+defaultDrops <- function(m, expected = 3000, upper.quant = 0.99, lower.prop = 0.1)
 # A function to call cells on library size, as performed by CellRanger
 # 
 # written by Jonathan Griffiths
 # created 4 January 2018
 {
   
-  if(upper > 1 | upper < 0){
+  if(upper.quant > 1 | upper.quant < 0){
     stop("'upper' should be a numeric value between 0 and 1")
   }
   
-  if(lower > 1 | lower < 0){
+  if(lower.prop > 1 | lower.prop < 0){
     stop("'lower' should be a numeric value between 0 and 1")
   }
   
   libs <- colSums(m)
   
-  top <- libs[order(libs, decreasing = TRUE)[seq_len(exp.cells)]]
+  o <- order(libs, decreasing = TRUE)
+  top <- libs[head(o, n = expected)]
   
-  threshold <- quantile(top, upper)*lower
+  threshold <- quantile(top, upper.quant)*lower.prop
   
   return(libs > threshold)
 }
--- tests/testthat/test-defaultdrops.R
+++ tests/testthat/test-defaultdrops.R
@@ -5,7 +5,7 @@ test_that("defaultDrops runs to completion", {
   #mock up counts
   source(system.file("scripts", "mock_empty.R", package="DropletUtils"))
   
-  out = defaultDrops(my.counts)
+  out <- defaultDrops(my.counts)
   #should always call at least one cell (100th %ile cell)
   expect_true(sum(out)>0)
 })
