0aa94d94bd653bc6f3f88013e2adbc8f1651a66a
Aaron Lun <alun@wehi.edu.au>
2018-01-08 10:28:36 +0000

    Switched back to direct numeric derivative for inflection point. Restricted spline to region around knee point for more accuracy. Added error message for insufficient points. Updated and cleaned up barcodeRanks documentation.

--- R/barcodeRanks.R
+++ R/barcodeRanks.R
@@ -1,4 +1,4 @@
-barcodeRanks <- function(m, lower=200, df=50, ...) 
+barcodeRanks <- function(m, lower=100, fit.bounds=NULL, df=20, ...) 
 # Returning statistics to construct a barcode-rank plot. Also calculates
 # the knee and inflection points for further use.
 #
@@ -13,24 +13,41 @@ barcodeRanks <- function(m, lower=200, df=50, ...)
     run.totals <- stuff$values
 
     keep <- run.totals > lower
+    if (sum(keep)<3) { 
+        stop("insufficient unique points for computing knee/inflection points")
+    } 
     y <- log10(run.totals[keep])
-    x <- log10(run.rank[keep]) 
+    x <- log10(run.rank[keep])
     
+    # Numerical differentiation to identify bounds for spline fitting.
+    # The upper/lower bounds are defined at the plateau and inflection, respectively.
+    d1n <- diff(y)/diff(x)
+    right.edge <- which.min(d1n)
+    left.edge <- which.max(d1n[seq_len(right.edge)])
+
+    # We restrict to this region, thereby simplifying the shape of the curve.
+    # This allows us to get a decent fit with low df for stable differentiation.
+    if (is.null(fit.bounds)) {
+        new.keep <- left.edge:right.edge
+    } else {
+        new.keep <- y > log10(fit.bounds[1]) & y < log10(fit.bounds[2])
+    }
+
     # Smoothing to avoid error multiplication upon differentiation.
-    fit <- smooth.spline(x, y, df=df, ...)
+    # Minimizing the signed curvature and returning the total for the knee point.
+    fit <- smooth.spline(x[new.keep], y[new.keep], df=df, ...)
     d1 <- predict(fit, deriv=1)$y
     d2 <- predict(fit, deriv=2)$y
-
-    # Minimizing the signed curvature and returning the total for the knee point.
     curvature <- d2/(1 + d1^2)^1.5
     knee <- 10^(y[which.min(curvature)])
 
-    # Minimizing the first derivative to get the total for the inflection point.
-    inflection <- 10^(y[which.min(d1)])
+    # Taking the right edge to get the total for the inflection point.
+    # We use the numerical derivative as the spline is optimized for the knee.
+    inflection <- 10^(y[right.edge])
 
     # Returning a whole stack of useful stats.
     fitted.vals <- rep(NA_real_, length(keep))
-    fitted.vals[keep] <- 10^fitted(fit)
+    fitted.vals[keep][new.keep] <- 10^fitted(fit)
     return(list(rank=.reorder(run.rank, stuff$lengths, o), 
                 total=.reorder(run.totals, stuff$lengths, o),
                 fitted=.reorder(fitted.vals, stuff$lengths, o),
