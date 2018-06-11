e8ba4fcc0ab7ce1bfc7d86a1634d65add89662d2
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-06-11 14:29:49 +0100

    Streamlined swappedDrops code to avoid copies.

--- R/swappedDrops.R
+++ R/swappedDrops.R
@@ -1,49 +1,46 @@
 #' @export
 #' @importFrom Matrix sparseMatrix
 swappedDrops <- function(samples, barcode.length=NULL, get.swapped=FALSE, get.diagnostics=FALSE, min.frac=0.8)
-# This function removes swapped reads between samples in 10X Genomics data.
+# Removes swapped reads between samples in 10X Genomics data.
 #
 # written by Jonathan Griffiths
 # with modifications from Aaron Lun
 # created 18 December 2017
 {
-    tabs <- lapply(samples, read10xMolInfo, barcode.length=barcode.length)
-    for (i in seq_along(tabs)) {
-        if (!identical(tabs[[1]]$genes, tabs[[i]]$genes)) {
+    ref.genes <- NULL
+    cells <- umis <- genes <- nreads <- vector("list", length(samples))
+
+    for (i in seq_along(samples)) {
+        mol.info <- read10xMolInfo(samples[i], barcode.length=barcode.length)
+
+        if (is.null(ref.genes)) {
+            ref.genes <- mol.info$genes            
+        } else if (!identical(ref.genes, mol.info$genes)) {
             stop("gene information differs between samples")
         }
 
-        curgems <- tabs[[i]]$data$gem_group
+        curgems <- mol.info$data$gem_group
         if (length(curgems) && min(curgems)!=max(curgems)) { 
-            warning("each sample should only contain GEM codes from one 10X run")
-            break
+            warning(paste0("sample '", samples[i], "' contains multiple GEM groups"))
         }
-        tabs[[i]]$data$gem_group <- NULL
-    }
-        
-    # Identifying swapped molecules.
-    cells <- umis <- genes <- nreads <- vector("list", length(samples))
-    for (i in seq_along(tabs)) {
-        current <- tabs[[i]]$data
+
+        current <- mol.info$data
         cells[[i]] <- current$cell
         umis[[i]] <- current$umi
         genes[[i]] <- current$gene
         nreads[[i]] <- current$reads
     }
-
-    cells <- unlist(cells)
-    umis <- unlist(umis)
-    genes <- unlist(genes)
-    nreads <- unlist(nreads)
-    swap.out <- .findSwapped(cells, umis, genes, nreads, min.frac, get.group=get.diagnostics)
+        
+    # Identifying swapped molecules.
+    all.nreads <- unlist(nreads)
+    swap.out <- .findSwapped(unlist(cells), unlist(umis), unlist(genes), all.nreads, min.frac, get.group=get.diagnostics)
     is.swap <- swap.out$swapped
 
     # Printing out per-molecule read counts. 
     if (get.diagnostics) {
-        sample.ids <- rep(seq_along(tabs), unlist(lapply(tabs, FUN=function(x) { nrow(x$data) })))
+        sample.ids <- rep(seq_along(nreads), lengths(nreads))
         nmolecules <- if (length(swap.out$group)) max(swap.out$group) else 0
-        diagnostics <- sparseMatrix(i=swap.out$group, j=sample.ids, x=nreads, 
-                                    dims=c(nmolecules, length(samples)))
+        diagnostics <- sparseMatrix(i=swap.out$group, j=sample.ids, x=all.nreads, dims=c(nmolecules, length(samples)))
         colnames(diagnostics) <- names(samples)
     }
 
@@ -51,21 +48,19 @@ swappedDrops <- function(samples, barcode.length=NULL, get.swapped=FALSE, get.di
     cleaned <- swapped <- vector("list", length(samples))
     names(cleaned) <- names(swapped) <- names(samples)
     last <- 0L
-    for (i in seq_along(tabs)) { 
-        current <- tabs[[i]]$data
-        all.genes <- tabs[[i]]$genes
-        all.cells <- sort(unique(current$cell))
+    for (i in seq_along(samples)) { 
+        all.cells <- sort(unique(cells[[i]]))
+        cur.cells <- cells[[i]]
+        cur.genes <- genes[[i]]
 
         # Forming count matrices from unswapped and swapped molecules. 
-        curswap <- is.swap[last + seq_along(current[[1]])]
-        cleaned[[i]] <- makeCountMatrix(current$gene[!curswap], current$cell[!curswap], 
-                                        all.genes=all.genes, all.cells=all.cells)
+        curswap <- is.swap[last + seq_along(cur.cells)]
+        cleaned[[i]] <- makeCountMatrix(cur.genes[!curswap], cur.cells[!curswap], all.genes=ref.genes, all.cells=all.cells)
         if (get.swapped) {
-            swapped[[i]] <- makeCountMatrix(current$gene[curswap], current$cell[curswap], 
-                                            all.genes=all.genes, all.cells=all.cells)
+            swapped[[i]] <- makeCountMatrix(cur.genes[curswap], cur.cells[curswap], all.genes=ref.genes, all.cells=all.cells)
         }
 
-        last <- last + length(current[[1]])
+        last <- last + length(cur.cells)
     }
 
     # Figuring out what kind of output to return.
