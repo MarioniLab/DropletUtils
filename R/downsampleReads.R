35f2ab2b62615e435d9702990904de00c5be2e01
Aaron Lun <aaron.lun@cruk.cam.ac.uk>
2018-08-18 14:24:08 +0100

    Refactored downsampleReads to use C++ cell/GEM ordering.

--- R/downsampleReads.R
+++ R/downsampleReads.R
@@ -6,26 +6,23 @@ downsampleReads <- function(sample, prop, barcode.length=NULL, bycol=FALSE)
 # written by Aaron Lun
 # created 19 December 2017    
 {
-    incoming <- read10xMolInfo(sample, barcode.length)
-    if (length(incoming$data$cell)) { 
-        cell.id <- paste0(incoming$data$cell, "-", incoming$data$gem_group)
-    } else {
-        cell.id <- character(0)
-    }
-    all.cells <- sort(unique(cell.id))
+    incoming <- read10xMolInfo(sample, barcode.length, get.umi=FALSE)
     
-    # Obtaining new read counts for each molecule.
-    o <- order(cell.id)
-    cell.id <- cell.id[o]
-    rout <- rle(cell.id)
+    out <- .get_cell_ordering(incoming$data$cell, incoming$data$gem_group)
+    o <- out$order
+    cell.id <- out$id
+    unique.cells <- out$cell
+    unique.gems <- out$gem
+    run.length <- out$length
 
     if (bycol) {
-        prop <- rep(prop, length.out=length(all.cells))
+        prop <- rep(prop, length.out=length(run.length))
     }
-    new.read.counts <- .Call(cxx_downsample_runs, rout$lengths, incoming$data$reads[o], prop, bycol)
+    new.read.counts <- .Call(cxx_downsample_runs, run.length, incoming$data$reads[o], prop, bycol)
 
     # Assembling into a sparse matrix.
     keep <- new.read.counts>0L
+    all.cells <- sprintf("%s-%i", unique.cells, unique.gems)
     makeCountMatrix(incoming$data$gene[o[keep]], cell.id[keep], 
                     all.genes=incoming$genes, all.cells=all.cells)
 }
