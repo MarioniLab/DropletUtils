0cd512b97cd649edc98b96b2b0755dbc8e5e09ca
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2019-01-08 09:54:05 +0000

    Avoid recreating gamma distribution object.

--- src/montecarlo_pval.cpp
+++ src/montecarlo_pval.cpp
@@ -62,9 +62,13 @@ SEXP montecarlo_pval (SEXP totalval, SEXP totallen, SEXP prob, SEXP ambient, SEX
         pcg32 generator(static_cast<uint32_t>(Seeds[it]), Streams[it]);
 
         if (use_alpha) {
+            typedef boost::random::gamma_distribution<double> distr_t;
+            typedef typename distr_t::param_type param_t;
+
+            // Do NOT cache gamma distribution across iterations, as this introduces possible dependencies.
+            distr_t cpp_gamma;
             for (size_t ldx=0; ldx<ngenes; ++ldx) {
-                // Do NOT cache across iterations, as this introduces possible dependencies.
-                tmpprob[ldx]=boost::random::gamma_distribution<double>(Ambient[ldx] * Alpha, 1)(generator);
+                tmpprob[ldx]=cpp_gamma(generator, param_t(Ambient[ldx] * Alpha, 1));
             }
             std::partial_sum(tmpprob.begin(), tmpprob.end(), cumprob.begin());
         }
