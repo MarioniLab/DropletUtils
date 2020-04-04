32ad4045dc8080860c207290a833010107235317
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-04-03 22:01:04 -0700

    Cleaned up documentation for hashedDrops.

--- R/hashedDrops.R
+++ R/hashedDrops.R
@@ -3,7 +3,7 @@
 #' Demultiplex cell barcodes into their samples of origin based on the most significant hash tag oligo (HTO).
 #' Also identify potential doublets based on the presence of multiple significant HTOs.
 #'
-#' Note that this function is still experimental and subject to change.
+#' Note that this function is still experimental; feedback is welcome.
 #'
 #' @param x A numeric/integer matrix-like object containing UMI counts.
 #' Rows correspond to HTOs and columns correspond to cell barcodes.
@@ -29,26 +29,29 @@
 #' Cells are mixed across multiple samples and subjected to droplet-based single-cell sequencing.
 #' Cell barcode libraries can then be demultiplexed into individual samples based on whether their unique HTO is detected.
 #'
-#' We identity the sample of origin for each barcode based on the HTO with the greatest deviation from the expected proportions.
-#' This is quantified by the ratio of the count for each HTO to the corresponding value of \code{prop}.
-#' In this manner, we account for potential imbalances in the distribution of HTOs in the multiplexed sample.
-#' The log-fold change in the largest and second-largest ratios is also reported for each barcode,
-#' with large log-fold changes representing confident assignment to a single sample.
+#' We identify the sample of origin for each cell barcode as that corresponding to the HTO with the greatest deviation from its expected proportion in the multiplexed pool.
+#' This deviation is quantified by the ratio of the count for each HTO to the corresponding value of \code{prop}.
+#' The log-fold change between the largest and second-largest ratios is also reported for each barcode, with large log-fold changes representing confident assignment to a single sample.
 #'
 #' We identify potential doublets by looking at the log-fold change in the second- and third-largest ratios.
 #' Large log-fold changes indicate that the second HTO is of higher abundance than the third HTO, consistent with a doublet. 
 #' Note that we assume that the counts for the third and remaining HTOs are due to background contamination;
 #' this ignores the (hopefully negligible) probability of higher-order multiplets.
 #'
-#' For both log-fold changes, we compute the number of MADs from the median across all cells.
-#' This allows users to easily identify doublets as large outliers in \code{NMAD.2to3}.
+#' For both log-fold changes, we compute the number of median absolute deviations (MADs) from the median across all cells.
+#' This allows users to easily identify doublets as large outliers, e.g., with \code{NMAD.2to3} above some threshold like 3 or 5.
 #' More sophisticated approaches may also consider \emph{small} outliers with negative values in \code{NMAD.1to2},
 #' where doublets will reduce the confidence of assignment to any single sample.
+#'
+#' We quantify deviation against \code{prop} to account for imbalances in the HTO distribution in the multiplexed sample.
+#' Ideally, \code{prop} would be set to the HTO profile in the ambient solution in order to capture deviations due to the presence of labelled cells.
+#' However, such information may not be available in many datasets where non-cell droplets are filtered out.
+#' As such, the default \code{prop} is instead defined from the row sums of \code{x} under the assumption that the rate of ambient to non-ambient transcripts is the same for each sample.
 #'
 #' @section Use only on non-empty droplets:
 #' This function assumes that cell calling has already been performed, e.g., with \code{\link{emptyDrops}}.
 #' Specifically, \code{x} should only contain columns corresponding to non-empty droplets.
-#' Log-fold changes for empty droplets will simply reflect stochasticity in the ambient solution,
+#' If empty droplets are included, their log-fold changes will simply reflect stochastic sampling in the ambient solution
 #' and violate the assumptions involved in interpreting the \code{NMAD} fields.
 #'
 #' If \code{x} contains columns for both empty and non-empty droplets,
@@ -86,6 +89,12 @@
 #' 
 #' # 'Best' contains the putative sample of origin.
 #' table(stats$Best, true.sample) 
+#'
+#' @references
+#' Stoeckius M, Zheng S, Houck-Loomis B et al. (2018)
+#' Cell Hashing with barcoded antibodies enables multiplexing and doublet detection for single cell genomics.
+#' \emph{Genome Biol.} 19, 1:224
+#'
 #' 
 #' @export
 #' @importFrom Matrix t colSums
