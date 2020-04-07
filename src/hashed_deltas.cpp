e2f967027d55a8c936e8407b606bc054351d4e60
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2020-04-06 23:44:39 -0700

    Modified C++ code for adjusting the ambient contamination.

--- src/hashed_deltas.cpp
+++ src/hashed_deltas.cpp
@@ -15,6 +15,7 @@ Rcpp::List hashed_deltas_internal(Rcpp::RObject mat, Rcpp::NumericVector prop, d
         throw std::runtime_error("'length(prop)' should be the same as 'nrow(mat)'");
     }
 
+    const double mean_prop=std::accumulate(prop.begin(), prop.end(), 0)/NR; 
     const int upto=std::min(NR, 3);
 
     Rcpp::IntegerVector output_best(NC), output_second(NC);
@@ -25,10 +26,44 @@ Rcpp::List hashed_deltas_internal(Rcpp::RObject mat, Rcpp::NumericVector prop, d
     for (int i=0; i<NC; ++i) {
         M->get_col(i, tmp.begin());
         for (int j=0; j<NR; ++j) {
-            collected[j].first=tmp[j]/prop[j] + pseudo_count;
+            collected[j].first=tmp[j]/prop[j];
             collected[j].second=j;
         }
 
+        // Estimating the scaling effect.
+        const int half=NR/2;
+        std::partial_sort(collected.begin(), collected.begin() + half + 1, 
+            collected.end(), std::greater<std::pair<double, int> >());
+
+        double scaling;
+        if (NR <= 3) {
+            scaling=collected[NR-1].first;
+        } else if (NR == 4) {
+            scaling=collected[2].first;
+        } else if ((NR % 2)==1) {
+            scaling=collected[half].first;
+        } else {
+            scaling=(collected[half-1].first + collected[half].first)/2;
+        }
+
+        /* First, subtracting the ambient solution. Then adding back a constant
+         * to be used as the pseudo-count. The product `scaling * mean_prop`
+         * represents the average ambient contamination for each HTO, so we just
+         * use it as a pseudo-count (scaled by the `pseudo_count` value). Nice 
+         * thing about this is that it adapts to the sequencing depth, under 
+         * some assumptions; see documentation for more details.
+         *
+         * We do require, at least, that the pseudo count be at least
+         * `pseudo_count`, just to avoid problems at `scaling=0`!
+         */
+        const double PSEUDO=std::max(1.0, scaling * mean_prop) * pseudo_count; 
+
+        for (auto& x : collected) {
+            x.first = tmp[x.second] - scaling * prop[x.second];
+            x.first = std::max(x.first, 0.0) + PSEUDO;
+        }
+
+        // Estimating the actual log-fold changes now. 
         std::partial_sort(collected.begin(), collected.begin()+upto,
             collected.end(), std::greater<std::pair<double, int> >());
 
