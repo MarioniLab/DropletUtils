777444a1856e992e50091e259e35fdf35b7e15c3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-26 00:24:08 -0700

    Refactor cellranger implementation to use closures.

--- R/emptyDropsCellRanger.R
+++ R/emptyDropsCellRanger.R
@@ -5,37 +5,28 @@
 #' 
 #' @param m A numeric matrix-like object containing counts, where columns represent barcoded droplets and rows represent features.
 #' The matrix should only contain barcodes for an individual sample, prior to any filtering for barcodes.
-#' 
 #' @param expected A numeric scalar specifying the expected number of barcodes in this sample It is same as \code{nExpectedCells} in STARsolo.
-#' 
 #' @param max.percentile A numeric scalar specifying the percentile used in simple filtering, barcodes selected by simple filtering 
-#' will be regarded as real barcodes regardless of the \code{emptyDrops} result. It is same as \code{maxPercentile} in STARsolo.
-#' 
+#' These will be regarded as real barcodes regardless of the \code{emptyDrops} result. 
+#' It is same as \code{maxPercentile} in STARsolo.
 #' @param max.min.ratio A numeric scalar specifying the maximum ratio of maximum UMI count and minimum UMI count used in simple filtering, 
 #' maximum UMI count used in simple filtering is determined first by \code{expected*(1-max.percentile)}, minimum UMI count used in
 #'  simple filtering is then determined by this ratio. It is same as \code{maxMinRatio} in STARsolo.
-#'  
 #' @param umi.min A numeric scalar specifying the minimum UMI count above which a sample will be included in ambient profiles. It is same as \code{umiMin} in STARsolo.
-#' 
 #' @param umi.min.frac.median A numeric scalar between 0 and 1 specifying that only the barcodes whose UMI count are above this number 
 #' fraction of the median UMI count of the top \code{expected} barcodes will be included in the ambient profile. 
 #' It is same as \code{umiMinFracMedian} in STARsolo.
-#' 
 #' @param cand.max.n An integer specifying the maximum number of ambient barcodes that are possible to be regarded as real barcodes. It is same as \code{canMaxN} in STARsolo.
-#' 
 #' @param ind.min An integer specifying the lowest UMI count ranking of the ambient pool, barcodes with UMI count ranking below
 #' this number will not be included in the ambient pool. It is same as \code{indMin} in STARsolo. It is also same as \code{by.rank} in \code{emptyDrops}.
-
 #' @param ind.max An integer specifying the highest UMI count ranking of the ambient pool, barcodes with UMI count ranking above
 #' this number will not be included in the ambient pool. It is same as \code{indMax} in STARsolo.
-#' 
 #' @param fdr A numeric scalar specifying the FDR threshold to filter barcodes. Barcodes whose FDR returned by emptyDrops
 #' is above this threshold will not be regarded as real barcodes. It is same as \code{FDR} in STARsolo.
-#'
 #' @param round Logical scalar indicating whether to check for non-integer values in \code{m} and, if present, round them for ambient profile estimation (see \code{?\link{ambientProfileEmpty}}) and the multinomial simulations.
 #' @param niters An integer scalar specifying the number of iterations to use for the Monte Carlo p-value calculations.
 #' @param BPPARAM A \linkS4class{BiocParallelParam} object indicating whether parallelization should be used.
-
+#'
 #' @details
 #' This function is an approximate implementation of the  \code{--soloCellFilter  EmptyDrops_CR} filtering approach of STARsolo 2.7.9a
 #' , which, itself, was reverse-engineered from the behavior of  CellRanger 3+. 
@@ -85,9 +76,6 @@
 #' @name emptyDropsCellRanger
 NULL
 
-# Authors: Dongze He, Rob Patro
-# Center of Bioinformatics and Computational Biology, University of Maryland, College Park, Maryland, 20740
-
 #' @export
 #' @rdname emptyDropsCellRanger
 
@@ -95,23 +83,29 @@ NULL
 #' @importFrom S4Vectors metadata<- metadata
 #' @importFrom BiocParallel SerialParam
 .empty_drops_cell_ranger  <- function(m, 
-                                      # STARsolo arguments
-                                      ## simple filtering
-                                      expected=3000,            # nExpectedCells
-                                      max.percentile=0.99,      # maxPercentile
-                                      max.min.ratio=10,         # maxMinRatio
-                                      ## emptyDrops_CR
-                                      umi.min=500,              # umiMin
-                                      umi.min.frac.median=0.01, # umiMinFracMedian
-                                      can.max.n=20000,          # candMaxN
-                                      ind.min=45000,            # indMin
-                                      ind.max=90000,            # indMax
-                                      fdr=0.01,                 # FDR
-                                      # emptyDrops arguments
-                                      round=TRUE,
-                                      niters=10000,
-                                      BPPARAM=SerialParam()
-) {
+    # STARsolo arguments
+    ## simple filtering
+    expected=3000,            # nExpectedCells
+    max.percentile=0.99,      # maxPercentile
+    max.min.ratio=10,         # maxMinRatio
+
+    ## emptyDrops_CR
+    umi.min=500,              # umiMin
+    umi.min.frac.median=0.01, # umiMinFracMedian
+    can.max.n=20000,          # candMaxN
+    ind.min=45000,            # indMin
+    ind.max=90000,            # indMax
+    fdr=0.01,                 # FDR
+
+    # emptyDrops arguments
+    round=TRUE,
+    niters=10000,
+    BPPARAM=SerialParam())
+{  
+    if (.bpNotSharedOrUp(BPPARAM)) {
+        bpstart(BPPARAM)
+        on.exit(bpstop(BPPARAM))
+    }
     
     # This function is an approximate implementation of the 
     # `--soloCellFilter  EmptyDrops_CR` filtering approach 
@@ -121,68 +115,65 @@ NULL
     # function is based can be found at (https://github.com/alexdobin/STAR/blob/master/source/SoloFeature_cellFiltering.cpp
     #  and https://github.com/alexdobin/STAR/blob/master/source/SoloFeature_emptyDrops_CR.cpp) 
     
-    ####################################################################################################################
-    # setup emptyDrops
-    if (!.bpNotSharedOrUp(BPPARAM)) {
-        bpstart(BPPARAM)
-        on.exit(bpstop(BPPARAM))
+    ambfun <- function(mat, totals) {
+        # Simple Filtering
+        # https://github.com/alexdobin/STAR/blob/master/source/SoloFeature_cellFiltering.cpp
+        # line 36-61
+        max.ind <- round(expected * (1 - max.percentile)) # maxind
+        n.umi.max <- totals[order(totals, decreasing = TRUE)[min(length(totals), max.ind)]] # nUMImax
+
+        # Barcodes with UMI count higher than retain will be regarded as real
+        # barcodes without any further tests
+        retain <- max(round(n.umi.max/max.min.ratio),1) # nUMImin
+
+        # select barcodes to use as ambient solution
+        # SoloFeature_emptyDrops_CR.cpp line 117-134
+        ncells.simple <- sum(totals >= retain)
+        min.umi  <- max(umi.min, round(umi.min.frac.median * totals[ncells.simple/2]))
+        i.cand.last <- min(ncells.simple + can.max.n, sum(totals > min.umi))
+        max.rank <- min(ind.max + 1, length(totals), i.cand.last)
+
+        # NOTE: parallelization handled by setAutoBPPARAM above.
+        discard <- rowSums(m) == 0
+        if (any(discard)) {
+            m <- m[!discard,,drop=FALSE]
+        }
+
+        o <- order(totals, decreasing=TRUE)
+        ambient <- logical(length(totals))
+        ambient[o[ind.min:max.rank]] <- TRUE
+        ambient.m <- m[,ambient,drop=FALSE]
+        ambient.prof <- rowSums(ambient.m)
+
+        if (sum(ambient.prof)==0) {
+            stop("no counts available to estimate the ambient profile")
+        }
+        ambient.prop <- .safe_good_turing(ambient.prof)
+
+        # Barcodes to keep are not just !ambient, as we want to exclude the
+        # barcodes that are too low to be even considered as ambient.
+        keep <- (o < ind.min) 
+
+        list(
+            m=m, # this MUST have the same number of columns as input.
+            discard=discard,
+            ambient=ambient,
+            ambient.m=ambient.m,
+            ambient.prop=ambient.prop,
+            keep=keep,
+            metadata=list(lower = totals[o[ind.min]], bottom=totals[o[max.rank]], retain=retain)
+        )
     }
-    
-    m <- .realize_DA_to_memory(m, BPPARAM)
-    m <- .rounded_to_integer(m, round)
 
-    ###################################################################################################################    
-    # Simple Filtering
-    # https://github.com/alexdobin/STAR/blob/master/source/SoloFeature_cellFiltering.cpp
-    # line 36-61
-    totals <- .intColSums(m)
-    
-    max.ind <- round(expected * (1 - max.percentile)) # maxind
-    n.umi.max <- totals[order(totals, decreasing = TRUE)[min(length(totals), max.ind)]] # nUMImax
-    ## retain: barcodes with UMI count higher than retain will be regarded as real barcodes without any further tests
-    retain <- max(round(n.umi.max/max.min.ratio),1) # nUMImin
-    ncells.simple <- sum(totals>=retain)
-    
-    ###################################################################################################################    
-    # select candidate barcodes
-    # SoloFeature_emptyDrops_CR.cpp line 117-134
-    
-    i.cand.first = ncells.simple+1
-    min.umi  <- max(umi.min, round(umi.min.frac.median * totals[ncells.simple/2]))
-    i.cand.last = min(ncells.simple + can.max.n, sum(totals > min.umi))
-
-    ## ignore: barcodes with UMI count higher than retain will be regarded as emptyDrops,
-    ## but barcodes with UMI counts between [ignore, lower] will be used in SimpleGoodTuring
-    ignore = totals[order(totals, decreasing = TRUE)[i.cand.last]]
-
-    stats <- .test_empty_drops(m=m, 
-                               lower=NULL, 
-                               bottom=NULL, 
-                               niters=niters, 
-                               test.ambient=FALSE, 
-                               ignore=ignore, 
-                               alpha=Inf, 
-                               round=FALSE, 
-                               by.rank=ind.min, 
-                               by.rank.bottom=ind.max, 
-                               retain=retain,
-                               BPPARAM=BPPARAM) 
+    stats <- .test_empty_drops(m=m, ambient.FUN=ambfun, niters=niters, test.ambient=FALSE, ignore=NULL, alpha=Inf, round=round, BPPARAM=BPPARAM)
 
     tmp <- stats$PValue
-    
-    # Possibly redefine 'lower' based on 'by.rank=' passed to testEmptyDrops.
-    lower <- metadata(stats)$lower
-    bottom <- metadata(stats)$bottom
-    
-    metadata(stats)$retain <- retain
+    retain <- metadata(stats)$retain
     always <- stats$Total >= retain
     tmp[always] <- 0
     
-    discard <- (stats$Total <= ignore)
-    tmp[discard] <- NA_real_
-    
     stats$FDR <- p.adjust(tmp, method="BH")
-    stats$is.cell = stats$FDR <= fdr
+    stats$is.cell <- stats$FDR <= fdr
     stats$is.cell[is.na(stats$is.cell)] <- FALSE
     stats
 }
@@ -199,5 +190,5 @@ setMethod("emptyDropsCellRanger", "ANY", .empty_drops_cell_ranger)
 #' @rdname emptyDropsCellRanger
 #' @importFrom SummarizedExperiment assay
 setMethod("emptyDropsCellRanger", "SummarizedExperiment", function(m, ..., assay.type="counts") {
-  .empty_drops_cell_ranger(assay(m, assay.type), ...)
+    .empty_drops_cell_ranger(assay(m, assay.type), ...)
 })
