171b77f2cf40542690346adf3d4156313d61841b
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-04-08 16:27:15 +1000

    Stripped out test comparisons to downsampleMatrix.
    These tests no longer make sense because the downsampling is no longer done on
    a per-event basis (i.e. single UMIs or reads in each count). Rather, the new
    algorithm operates on the entire count at once, for greater efficiency.
    
    This all means that UMI and read counts are no longer interchangeable, which
    means that the results from downsampleReads/Matrix are no longer directly
    comparable. This is okay - the test itself was highly contrived anyway as it
    only works when every UMI has a single read.

--- tests/testthat/test-downsample.R
+++ tests/testthat/test-downsample.R
@@ -53,50 +53,6 @@ test_that("downsampling from the reads yields correct results", {
     out <- downsampleReads(out.paths, barcode, prop=0.5)
     expect_identical(dim(out), c(0L, 0L))
 })
-    
-test_that("downsampling from the reads compares correctly to downsampleMatrix", {
-    # Manually creating files for comparison to downsampleMatrix - this relies on ordered 'gene' and 'cell', 
-    # so that the retention probabilities applied to each molecule are the same across functions.
-    ngenes <- 4
-    gene.count <- seq_len(ngenes)*100
-    ncells <- 200
-    nmolecules <- sum(gene.count)*ncells
-
-    tmpfile <- tempfile()
-    dir.create(tmpfile)
-    out.file <- file.path(tmpfile, "out.h5")
-
-    library(rhdf5)
-    h5 <- h5createFile(out.file)
-    h5write(rep(seq_len(ncells), each=sum(gene.count)), out.file, "barcode")
-    h5write(seq_len(nmolecules), out.file, "umi")
-    h5write(rep(rep(seq_len(ngenes)-1L, gene.count), ncells), out.file, "gene")
-    h5write(rep(1, nmolecules), out.file, "gem_group")
-    h5write(rep(1, nmolecules), out.file, "reads") # one read per molecule.
-    h5write(array(sprintf("ENSG%i", seq_len(ngenes))), out.file, "gene_ids")
-
-    alt <- read10xMolInfo(out.file)
-    X <- makeCountMatrix(alt$data$gene, alt$data$cell, all.genes=alt$genes)
-    colnames(X) <- paste0(colnames(X), "-1")
-
-    set.seed(100)
-    Y <- downsampleReads(out.file, prop=0.11)
-    expect_equal(dim(Y), dim(X))
-    expect_equal(rownames(Y), rownames(X))
-    expect_equal(colnames(Y), colnames(X))
-    expect_true(all(Y >= 0))
-    expect_true(min(X - Y) >= 0)
-    expect_equal(sum(Y), round(0.11 * sum(X)))
-
-    set.seed(100)
-    Y <- downsampleReads(out.file, prop=0.55, bycol=TRUE)
-    expect_equal(dim(Y), dim(X))
-    expect_equal(rownames(Y), rownames(X))
-    expect_equal(colnames(Y), colnames(X))
-    expect_true(all(Y >= 0))
-    expect_true(min(X - Y) >= 0)
-    expect_equal(colSums(Y), round(0.55 * colSums(X)))
-})
 
 test_that("downsampling from the reads works correctly with feature subsets", {
     tmpfile <- tempfile(fileext=".h5")
